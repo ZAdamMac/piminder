8805aca38d6770118492823f8ed9d395c2318711
zadammac <zadammac@kenshosec.com>
2021-04-28 21:34:45 -0300

    The pip-install commit! This makes all the packages pip-installable and executable accordingly. Not tested with service as service has a differently-accessed ultimate execution path.

---
+++ src/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("../README.md", "r") as f:
+      long_description = f.read()
+
+setup(name='pyminder',
+      version='0.2.0',
+      description='Scripts for reporting errors to a pyminder service',
+      long_description=long_description,
+      author='Zac Adam-MacEwen',
+      author_email='zadammac@kenshosec.com',
+      url='https://www.github.com/zadammac/pyminder',
+      packages=['pyminder_helpers', 'pyminder_monitor', 'pyminder_service'],
+      install_requires=['bcrypt', 'flask', 'flask_restful', 'pymysql'],
+      long_description_content_type="text/markdown"
+     )
